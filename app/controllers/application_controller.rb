2002aecfa1d22ec2255bc7169f8ffa00e7c6f29d
rocksTaa <rockstaa48@yahoo.com>
2023-09-22 22:13:22 +0300

    create cart and orders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :render_cart
+
+  def render_cart
+    @render_cart = true
+  end
+
+  def current_cart
+    if current_user.present?
+      current_user.current_cart
+    else
+      Cart.find(session[:cart_id])
+    end
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart_id
+  end
 end
