34b7e8d053e5df3762efe5cffa4954d54c03ec45
rocksTaa <rockstaa48@yahoo.com>
2023-09-22 20:38:41 +0300

    car and order set

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,5 @@
 class Cart < ApplicationRecord
+  belongs_to :user
+  has_many :orders, dependent: :destroy
+  has_many :items, through: :orders
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :subcategory
+  has_many :orders
+  has_many :carts, through: :orders
+  belongs_to :user
 end
