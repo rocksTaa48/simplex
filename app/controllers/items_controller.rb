4cb3e99413ffb4a5a3d9f0bfe9660ff4c65a264e
rocksTaa <rockstaa48@yahoo.com>
2023-10-31 22:32:33 +0300

    before_create_stores

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find_by(id: params[:id])
     params[:current_category] = @item.subcategory.category.id
-    params[:current_subcategory] =@item.subcategory.id
+    params[:current_subcategory] = @item.subcategory.id
     current_subcategory = @item.subcategory.category&.subcategories
     @categories = Category.all
     @subcategories = @category&.subcategories || current_subcategory
@@ -52,12 +52,13 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def submit_catalogue
     @category = Category.find_by(id: params[:category].presence)
     @subcategory = Subcategory.find_by(id: params[:subcategory].presence)
   end
 
   def item_params
-    params.require(:item).permit(:title, :description, :quantity, :price, :category_id, :subcategory_id, :user_id)
+    params.require(:item).permit(:title, :description, :quantity, :price, :subcategory_id, :user_id)
   end
 end
